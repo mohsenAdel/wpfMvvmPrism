6d753173774fa3dfce25415c4dfd256c110af67b
mohsen <mohsenadel6@gmail.com>
2016-09-27 06:05:47 +0200

    Create Basic init for wpf mvvm with Prism

---
+++ wpfmvvm/ViewModels/ViewaViewModel.cs
@@ -0,0 +1,81 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace wpfmvvm.ViewModels
+{
+    class ViewaViewModel:BindableBase
+    {
+        private string _FirstName;
+            public string FirstName
+        {
+            get
+            {
+                return _FirstName;
+            }
+
+            set
+            {
+               
+               
+                SetProperty(ref _FirstName, value);
+                //updateCommand.RaiseCanExecuteChanged();
+            }
+        }
+
+
+        private string _LastName;
+        public string LastName
+        {
+            get
+            {
+                return _LastName;
+            }
+
+            set
+            {
+
+                SetProperty(ref _LastName, value);
+                updateCommand.RaiseCanExecuteChanged();
+            }
+        }
+
+
+        private DateTime? _LastUpdate;
+        public DateTime? LastUpdate
+        {
+            get
+            {
+                return _LastUpdate;
+            }
+
+            set
+            {
+
+                SetProperty(ref _LastUpdate, value);
+            }
+        }
+
+        public DelegateCommand updateCommand { get; set; }
+
+        public ViewaViewModel()
+        {
+            updateCommand = new DelegateCommand(execute, CanExcute);
+        }
+
+        private bool CanExcute()
+        {
+            return !string.IsNullOrWhiteSpace(FirstName) &&!string.IsNullOrWhiteSpace(LastName);
+        }
+
+        private void execute()
+        {
+            LastUpdate = DateTime.Now;
+        }
+    }
+}
---
+++ wpfmvvm/Views/Viewa.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using wpfmvvm.ViewModels;
+
+namespace wpfmvvm.Views
+{
+    /// <summary>
+    /// Interaction logic for Viewa.xaml
+    /// </summary>
+    public partial class Viewa : UserControl
+    {
+        public Viewa()
+        {
+            InitializeComponent();
+            DataContext = new ViewaViewModel();
+        }
+    }
+}
