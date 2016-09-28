6f787e151e2eefd7d90082f85cff60c2bbde45de
mohsen <mohsenadel6@gmail.com>
2016-09-28 19:03:05 +0200

    add navigation using prism.unity with made new class for bootstraber

---
+++ wpfmvvm/Bootstarpper.cs
@@ -0,0 +1,33 @@
+﻿using Prism.Unity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Microsoft.Practices.Unity;
+
+namespace wpfmvvm
+{
+    class Bootstarpper:UnityBootstrapper
+    {
+
+        protected override DependencyObject CreateShell()
+        {
+            return Container.Resolve<Views.MainWindow>();
+        }
+
+        protected override void InitializeShell()
+        {
+            Application.Current.MainWindow.Show();
+        }
+
+        protected override void ConfigureContainer()
+        {
+            base.ConfigureContainer();
+
+            Container.RegisterType(typeof(object), typeof(Views.Viewa), "Viewa");
+            Container.RegisterType(typeof(object), typeof(Views.Viewb), "Viewb");
+        }
+    }
+}
---
+++ wpfmvvm/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,28 @@
+﻿using Prism.Mvvm;
+using Prism.Commands;
+using Prism.Regions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wpfmvvm.ViewModels
+{
+  public  class MainWindowViewModel: BindableBase
+    {
+        private readonly IRegionManager _RegionManager;
+        public DelegateCommand<String> NavigateCommand { get; set; }
+
+        public MainWindowViewModel(IRegionManager RegionManager)
+        {
+            _RegionManager = RegionManager;
+            NavigateCommand = new DelegateCommand<string>(Navigate);
+        }
+
+        private void Navigate(string Uri)
+        {
+            _RegionManager.RequestNavigate("ContentRegion", Uri);
+        }
+    }
+}
